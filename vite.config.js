66a451af577f6ec35fe0033d4b7bc8e39d9f0a49
Minahil Rabbani <minahilrabbani5@gmail.com>
2026-08-11 18:10:46 +0500

    Fix Tailwind CSS styling

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
 
 // export default defineConfig({
-//   plugins: [react(), tailwindcss()],
+//   plugins: [react()],
+//   base: "/punjab-college/",
 // });
+
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/punjab-college/",
 });
